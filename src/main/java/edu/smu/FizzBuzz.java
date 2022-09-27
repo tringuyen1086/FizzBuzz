f82f0e5031742a27344595174566d5c2e280abde
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-09-26 23:13:13 -0700

    added codes for FizzBuzz

--- src/main/java/edu/smu/FizzBuzz.java
+++ src/main/java/edu/smu/FizzBuzz.java
@@ -1,2 +1,26 @@
-package edu.smu;public class FizzBuzz {
+package edu.smu;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter your desired number n = ");
+        int num = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i <= num; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.print("FizzBuzz, ");
+            } else if (i % 3 == 0){
+                System.out.print("Fizz, ");
+            } else if (i % 5 == 0){
+                System.out.print("Buzz, ");
+            } else {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
 }
